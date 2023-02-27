d05310815a233304070f968adb0981a5487eab6d
charalampos <charalampos.mylothros@navd.ai>
2023-02-27 21:28:49 +0200

    fix develop branch

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
-import rootReducers from './reducers';
+import rootReducers from "./reducers";
 
 const initialState = {};
 const midleware = [thunk];
 
-export const store = createStore(
-    rootReducers,
-    initialState,
-    composeWithDevTools(applyMiddleware(...midleware))
-);
+export const store = createStore(rootReducers, initialState, composeWithDevTools(applyMiddleware(...midleware)));
 
 export default store;
