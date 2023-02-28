782f2f2c859cee54c21a9aa3cd5306f6feaed6e9
charalampos <charalampos.mylothros@navd.ai>
2023-03-01 01:43:40 +0200

    add paginate component

--- src/component/content/main-content/MainContent.js
+++ src/component/content/main-content/MainContent.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './MainContent.scss'
 import Slideshow from '../slideshow/Slideshow'
+import Paginate from '../paginate/Paginate'
 
 const MainContent = () => {
     const images = [
@@ -14,14 +15,27 @@ const MainContent = () => {
         {
             url: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
         }
-    ]
+    ];
+
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const paginate = (type) => {
+        if (type === 'prev' && currentPage > 1) {
+            setCurrentPage((prev) => prev - 1)
+        }
+        else {
+            setCurrentPage((prev) => prev + 1)
+        }
+    }
 
     return (
         <div className="main-content">
             <Slideshow images={images} auto={true} showArrows={true} />
             <div className="grid-movie-title">
                 <div className="movieType">Now Playing</div>
-                <div className="paginate">Paginate</div>
+                <div className="paginate">
+                    <Paginate paginate={paginate} totalPages={20} currentPage={currentPage}/>
+                </div>
             </div>
         </div>
     )
