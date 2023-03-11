cd26db85edd8f658ebdabb705b8de3edb9b77660
charalampos <charalampos.mylothros@navd.ai>
2023-03-12 01:32:02 +0200

    fix ) in reviews

--- src/component/content/details/reviews/Reviews.js
+++ src/component/content/details/reviews/Reviews.js
@@ -24,7 +24,7 @@ const Reviews = (props) => {
               </div>
             )
           }) :
-          <p>No reviews are posted yet.)</p>
+          <p>No reviews are posted yet.</p>
           }
       </div>
     </>
