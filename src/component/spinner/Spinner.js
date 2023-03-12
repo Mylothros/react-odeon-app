4b6254c7060824fae49a399486e5afb1a4acbcb1
charalampos <charalampos.mylothros@navd.ai>
2023-03-12 02:03:00 +0200

    add spinner test and delete app test

--- src/component/spinner/Spinner.js
+++ src/component/spinner/Spinner.js
@@ -4,11 +4,10 @@ import './Spinnes.scss';
 
 const Spinner = () => {
     return (
-        <div className="spinner">
+        <div className="spinner" data-testid="spinner">
             <div className="bounce1"></div>
             <div className="bounce2"></div>
             <div className="bounce3"></div>
-            
         </div>
     )
 }
